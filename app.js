75b08612fab638905f64b0491f21bc848ce702ef
323266536 <phillyp.henning@gmail.com>
2019-08-12 10:11:11 -0400

    Added first page (index) and created app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var chalk = require('chalk');
+var morgan = require('morgan');
+var debug = require('debug')('app');
+var path = require('path');
+
+var app = express();
+
+// Middleware
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist/')));
+
+// Routes
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(3000, function (){
+    debug(`Listening port ${chalk.green('3000')}`);
+});

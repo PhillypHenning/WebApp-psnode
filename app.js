46df64ee735bb294c104df79f35f10a1ec4025e6
323266536 <phillyp.henning@gmail.com>
2019-08-13 13:59:05 -0400

    implemented books and readied single book page router and ahref

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 
 // Middleware
 app.use(morgan('tiny'));
@@ -17,18 +18,63 @@ app.set('views', './src/views/');
 //app.set('view engine', 'pug');
 app.set('view engine', 'ejs');
 
-// Routes
-app.get('/', (req, res) => {
-  //res.sendFile(path.join(__dirname, 'views', 'index.html'));
-  res.render('index', 
+var books = [
     {
-      nav: [{ link: '/books', title: 'Books'},
-            { link: '/authors', title:'Authors'}], 
-      title: 'Library'
+        "author": "Chinua Achebe",
+        "country": "Nigeria",
+        "language": "English",
+        "pages": 209,
+        "title": "Things Fall Apart",
+        "year": 1958
+    },
+
+    {
+        "author": "Hans Christian Andersen",
+        "country": "Denmark",
+        "language": "Danish",
+        "pages": 784,
+        "title": "Fairy tales",
+        "year": 1836
+    },
+
+    {
+        "author": "Dante Alighieri",
+        "country": "Italy",
+        "language": "Italian",
+        "pages": 928,
+        "title": "The Divine Comedy",
+        "year": 1315
+    }
+];
+
+// Routes
+// Sets up the root of `/books/`
+bookRouter.route('/')
+    .get((req, res) => {
+        res.render('books',
+            {
+                nav: [{ link: '/books', title: 'Books' },
+                { link: '/authors', title: 'Authors' }],
+                title: 'Library', 
+                books
+            }
+        );
     });
+
+bookRouter.route('/single').get((req, res) => { res.send('single book'); });
+
+app.use('/books', bookRouter);
+app.get('/', (req, res) => {
+    //res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.render('index',
+        {
+            nav: [{ link: '/books', title: 'Books' },
+            { link: '/authors', title: 'Authors' }],
+            title: 'Library'
+        });
 });
 
 // Server
 app.listen(port, () => {
-  debug(`Listening on port ${chalk.yellow(port)}`);
+    debug(`Listening on port ${chalk.yellow(port)}`);
 });

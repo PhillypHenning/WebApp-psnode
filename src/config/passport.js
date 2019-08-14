326f29dd563193bb978a9bb3563fb97aa7c4a328
323266536 <phillyp.henning@gmail.com>
2019-08-14 14:42:52 -0400

    passport + local.strategy setup

---
+++ src/config/passport.js
@@ -0,0 +1,20 @@
+const passport = require('passport');
+const debug = require('debug')('app:passport');
+require('./strategies/local.strategy');
+
+module.exports = function passportConfig(app) {
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // Stores a user in the session
+  passport.serializeUser((user, done)=>{
+    done(null, user);
+  });
+
+
+  // Retrieves a user from the sesssion 
+  passport.deserializeUser((user, done) => {
+    debug(`User retrieved: ${user}`)
+    done(null, user);
+  });
+};

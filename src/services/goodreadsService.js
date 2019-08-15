4ce9d0c574c91255bb65272c871913186acb5eb4
323266536 <phillyp.henning@gmail.com>
2019-08-15 14:39:39 -0400

    adding axios and xml2js

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,7 +1,26 @@
+const debug = require('debug')('app:goodreadsService');
+const xml2js = require('xml2js');
+const axios = require('axios');
+
+const parser = xml2js.Parser({ explicitArray: false });
 function goodreadService() {
-  function getBookById() {
+  function getBookById(id) {
     return new Promise((resolve, reject) => {
-      resolve({ description: 'our description' });
+      axios.get('https://www.goodreads.com/book/show/50.xml?key=0V1ssJHuSAW8H4p74zhA')
+        .then((response) => {
+          parser.parseString(response.data, (err, result) => {
+            if (err) {
+              debug(err);
+            } else {
+              debug(result);
+              resolve(result.GoodreadsResponse.book);
+            }
+          })
+        })
+        .catch((error) => {
+          reject(error);
+          debug(error);
+        })
     })
   }
   return { getBookById }
